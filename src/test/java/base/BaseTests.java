971b8e586e4302300f8221d8e36df0b13175ad1d
TStarboard <tswong@gmail.com>
2019-10-30 11:33:31 -0700

    Push test files to github

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -13,7 +13,7 @@ public class BaseTests {
 
     @BeforeClass
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
