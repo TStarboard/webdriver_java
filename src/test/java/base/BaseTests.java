244baf97aece3ce748a293de4b325fd61f349157
TStarboard <tswong@gmail.com>
2019-10-29 17:18:46 -0700

    Added LoginTests

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,26 +1,44 @@
 package base;
 
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeClass
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        driver.manage().window().maximize();
+        homePage = new HomePage(driver);
+
+		/*List<WebElement> links = driver.findElements(By.tagName("a"));
+        System.out.println(links.size());
+
+        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
 
         System.out.println(driver.getTitle());
+        */
+
+
+    }
+
+    @AfterClass
+    public void tearDown(){
         driver.quit();
     }
 
-    public static void main(String[] args) {
+   /*   NOTE: no logner need this section because of using TestNG structure of above @AfterClass
+        public static void main(String args[]){
         BaseTests test = new BaseTests();
         test.setUp();
-    }
+    }*/
 }
